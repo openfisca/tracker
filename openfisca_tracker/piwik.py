e35940fb948bf70116c16448b2b0268aa97838d8
sandcha <sandcha@users.noreply.github.com>
2017-07-12 00:58:28 +0200

    Minor correction and cleaning

--- openfisca_tracker/piwik.py
+++ openfisca_tracker/piwik.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 
 from urllib import urlencode
-
 from unirest import post
 
-# Piwik parameters:
 
-PIWIK_URL = "https://openfisca.innocraft.cloud/piwik.php"
-ID_SITE = 1
+class PiwikTracker:
 
+    url = None  # Piwik tracking http api endpoint. 
+    idsite = None  # Piwik id of the tracked website.
 
-# TODO: Use action_name to create a category/api version number?
-def track(action_url, callback = None):
-    params = {'idsite': ID_SITE, 'rec': 1, 'url': action_url }
-    post(PIWIK_URL, params = params, callback = callback)
+    def __init__(self, url, idsite):
+        self.url = url
+        self.idsite = idsite
+
+    def track(self, action_url, callback = None):
+        params = {'idsite': self.idsite, 'rec': 1, 'url': action_url }
+        post(self.url, params = params, callback = callback)
