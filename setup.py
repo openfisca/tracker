48b23e26ceb4f7e5c4e324d4a05dc5a2394441eb
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-07-11 20:00:19 +0200

    Rename distribution

--- setup.py
+++ setup.py
@@ -4,16 +4,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Tracker",
+    name="OpenFisca-Tracker",
     version="0.0.1",
     description="A Tracker of the OpenFisca Web API usage",
     license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
     author="",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'OpenFisca-Web-API >= 6.0.0',
-        ],
+    install_requires=[],
     extras_require={
         'test': [
             'nose',
