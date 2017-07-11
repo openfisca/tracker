4f8d93a190622262245d7d5515a0fe16a0066d5a
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-07-11 19:23:08 +0200

    Clean code

--- tracker/piwik.py
+++ tracker/piwik.py
@@ -1,50 +1,16 @@
 # -*- coding: utf-8 -*-
 
-import os
-import pkg_resources
-
-import logging
-import ConfigParser
-
 from urllib import urlencode
-from urlparse import urljoin
 
 from unirest import post
 
-# TODO: Bulk tracking (from flask import jsonify)
-# {
-#    "requests": [
-#       "?idsite=1&url=http://example.org&action_name=Test bulk log Pageview&rec=1",
-#       "?idsite=1&url=http://example.net/test.htm&action_name=Another bul k page view&rec=1"
-#    ],
-#    "token_auth": "33dc3f2536d3025974cccb4b4d2d98f4"
-# }
-
-CONF_FILE_NAME = 'config/config.ini'
-log = logging.getLogger(__name__)
-
-# TODO: Clean? application = None
-# TODO: Clean? urls = []
-
 # Piwik parameters:
 
-URL="https://openfisca.innocraft.cloud/piwik.php"
-idsite=1
-rec=1
+PIWIK_URL = "https://openfisca.innocraft.cloud/piwik.php"
+ID_SITE = 1
+
 
 # TODO: Use action_name to create a category/api version number?
-# TODO: Track unique visitors with _id?
-
-def new_piwik_url(url):  # The full URL for the current action.
-    params = urlencode(get_piwik_params(url))
-    return u'https://openfisca.innocraft.cloud/piwik.php?' + params
-
-def get_piwik_params(url):
-    return {'idsite': idsite, 'rec': rec, 'url': url }
-
-# In case of timeout or any other exception during the connection of the request:
-def exception_handler(request, exception):
-    log.error("Request failed")
-
-def track(action_url, callback=None):
-    post(URL, params=get_piwik_params(action_url), callback=callback)
+def track(action_url, callback = None):
+    params = {'idsite': ID_SITE, 'rec': 1, 'url': action_url }
+    post(PIWIK_URL, params = params, callback = callback)
--- tracker/tests/test_piwik.py
+++ tracker/tests/test_piwik.py
@@ -6,44 +6,13 @@ from nose.tools import assert_equal, assert_true
 
 from tracker.piwik import *
 
-# TODO: test with old and preview api app...
 
-openfisca_urls = [
-    'http://127.0.0.1:2000/',
-    'http://127.0.0.1:2000/api/2/entities',
-    'http://127.0.0.1:2000/api/2/formula/2017-02/cout_du_travail?salaire_de_base=2300',
-    'http://127.0.0.1:2000/api/1/parameters?name=impot_revenu.bareme',
-    'http://127.0.0.1:2000/api/1/reforms',
-    'http://127.0.0.1:2000/api/1/variables?name=irpp'
-    ]
-
-# curl http://127.0.0.1:2000/api/1/calculate -X POST --data @./tests/json/test_calculate.json --header 'Content-type: application/json'
-# curl http://127.0.0.1:2000/api/1/simulate -X POST --data @./tests/json/test_simulate.json --header 'Content-type: application/json'
-
-def test_track_unirest():
+# Doesn't check that the action was actually tracked, just that it doesn't crash. You can manually check online at PIWIK_URL that the action was indeed tracked.
+def test_track():
     thread = track('https://api.openfisca.fr')
 
-# def test_send_requests():
-#     requests = set_requests(openfisca_urls)
-#     for req in requests:
-#         assert req is not None, req
-#         assert req.response is None
 
-#     send_all_and_handle(requests, exception_handler)  # responses
-
-#     for req in requests:
-#         assert False, req.response
-
-# #    for res in responses:
-# #        assert res is not None, res
-
-
-def test_new_piwik_url():
-    piwik_url = new_piwik_url('https://api.openfisca.fr')
-    assert_equal(piwik_url, "https://openfisca.innocraft.cloud/piwik.php?url=https%3A%2F%2Fapi.openfisca.fr&rec=1&idsite=1")
-
-
-def test_send_callback():
+def test_track_callback():
     log = []
     def callback(response):
         log.append('callback')
