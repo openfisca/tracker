0c24553499439b0e585835cc5a9c7192c364b34e
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-07-13 19:12:56 +0200

    Add timer

--- openfisca_tracker/piwik.py
+++ openfisca_tracker/piwik.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import json
+from threading import Lock, Timer
 
 from unirest import post
 
-BUFFER_SIZE = 10
+BUFFER_SIZE = 15  # We send the tracked requests by group
+TIMER_INTERVAL = 3600  # We send the tracked requests every TIMER_INTERVAL seconds
 
 
 def default_callback(response):
@@ -12,20 +14,22 @@ def default_callback(response):
 
 
 class PiwikTracker:
-
-    url = None  # Piwik tracking http api endpoint.
-    idsite = None  # Piwik id of the tracked website.
-
     def __init__(self, url, idsite):
-        self.url = url
-        self.idsite = idsite
-        self.requests = []
+        self.url = url  # Piwik tracking http api endpoint
+        self.idsite = idsite  # Piwik id of the tracked website
+        self.requests = []  # Awaiting tracked requests
+        self.lock = Lock()
+        self.start_timer()
 
+    def start_timer(self):
+        Timer(TIMER_INTERVAL, self.stop_timer).start()
 
-    def track(self, action_url):
-        tracked_request = "?idsite={}&url={}&rec=1".format(self.idsite, action_url)
-        self.requests.append(tracked_request)
-        if len(self.requests) == BUFFER_SIZE:
+    def stop_timer(self):
+        self.send()
+        self.start_timer()
+
+    def send(self):
+        with self.lock:
             post(
                 self.url,
                 headers = { "Accept": "application/json" },
@@ -33,3 +37,10 @@ class PiwikTracker:
                 callback = default_callback
                 )
             self.requests = []
+
+    def track(self, action_url):
+        tracked_request = "?idsite={}&url={}&rec=1".format(self.idsite, action_url)
+        with self.lock:
+            self.requests.append(tracked_request)
+        if len(self.requests) == BUFFER_SIZE:
+            self.send()
