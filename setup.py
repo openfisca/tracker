b4447d41006d7e0c351267dca6fad7ffc6fe960d
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-07-31 21:14:46 +0200

    Add missing dependency

--- setup.py
+++ setup.py
@@ -5,13 +5,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="OpenFisca-Tracker",
-    version="0.0.1",
+    version="0.1.0",
     description="A Tracker of the OpenFisca Web API usage",
     license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
     author="",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[],
+    install_requires=[
+        'unirest == 1.1.7'
+        ],
     extras_require={
         'test': [
             'nose',
