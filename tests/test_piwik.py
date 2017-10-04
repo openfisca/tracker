50bbbb812be30b6e9d8cae936a75d61165518df2
Anna-Livia <annalivia.cardin@gmail.com>
2017-10-04 15:50:47 +0200

    Update tests

--- tests/test_piwik.py
+++ tests/test_piwik.py
@@ -1,21 +1,19 @@
 # -*- coding: utf-8 -*-
-
-import time
-
-from nose.tools import assert_equal
-
 from openfisca_tracker.piwik import PiwikTracker, BUFFER_SIZE
 
 TRACKER_URL = 'https://stats.data.gouv.fr/piwik.php'
 TRACKER_IDSITE = 4
+TRACKER_AUTH = ""
+
 FAKE_ACTION_URL = 'https://test-tracking.openfisca.fr'
+FAKE_ACTION_IP = '111.11.1.1'
 
 
-tracker = PiwikTracker(TRACKER_URL, TRACKER_IDSITE)
+tracker = PiwikTracker(TRACKER_URL, TRACKER_IDSITE, TRACKER_AUTH)
 
 
 # Doesn't check that the action was actually tracked, just that it doesn't crash.
 # You can manually check online at TRACKER_URL that the action was indeed tracked.
 def test_track():
     for i in range(BUFFER_SIZE):
-        tracker.track(FAKE_ACTION_URL + '/test_track')
+        tracker.track(FAKE_ACTION_URL + '/test_track', TRACKER_AUTH)
