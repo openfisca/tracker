553baf55a9fac9b47c9667398c5532b783568710
sandcha <sandcha@users.noreply.github.com>
2019-03-26 12:28:22 +0100

    Clean up test

--- tests/test_piwik.py
+++ tests/test_piwik.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from openfisca_tracker.piwik import PiwikTracker, BUFFER_SIZE, TIMER_INTERVAL
+from openfisca_tracker.piwik import PiwikTracker, BUFFER_SIZE
 import pytest
 
 TRACKER_URL = 'https://stats.data.gouv.fr/piwik.php'
@@ -12,7 +12,7 @@ FAKE_ACTION_IP = '111.11.1.1'
 
 class TestPiwikTracker(PiwikTracker):
     def __init__(self, url, idsite, token_auth):
-       super().__init__(url, idsite, token_auth)
+        super().__init__(url, idsite, token_auth)
 
     def start_timer(self):
         pass
@@ -26,29 +26,22 @@ def tracker():
     tracker = TestPiwikTracker(TRACKER_URL, TRACKER_IDSITE, TRACKER_AUTH)
     yield tracker
     print("bye")
-    # TIMER_INTERVAL = 0
-    # tracker = None
 
 
-# Doesn't check that the action was actually tracked, just that it doesn't crash.
 # You can manually check online at TRACKER_URL that the action was indeed tracked.
-# This test never exits. It needs no be manually exited (ctrl + c).
-# It might be caused by the timer thread in the piwik.py file.
 
-# @pytest.mark.timeout(1)
+
 def test_track(tracker):
     for i in range(BUFFER_SIZE):
         tracker.track(FAKE_ACTION_URL + '/test_track', FAKE_ACTION_IP)
     assert len(tracker.requests) == 0
 
 
-# @pytest.mark.timeout(1)
 def test_track_with_api_version(tracker):
     for i in range(BUFFER_SIZE):
         tracker.track(FAKE_ACTION_URL + '/test_track', FAKE_ACTION_IP, "test_country_package")
 
 
-# @pytest.mark.timeout(1)
 def test_track_with_action(tracker):
     for i in range(BUFFER_SIZE):
         tracker.track(FAKE_ACTION_URL + '/test_track', FAKE_ACTION_IP, "test_country_package", "/test_action")
