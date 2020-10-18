868634f45e4147e57b017db7b80fe4b7ab510b76
Mauko Quiroga <hello@mauko.me>
2020-10-19 00:59:40 +0200

    Add flake8-print

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
             "flake8-comprehensions >= 3.2.0, < 4.0.0",
             "flake8-docstrings >= 1.5.0, < 2.0.0",
             "flake8-import-order >= 0.18.0, < 1.0.0",
+            "flake8-print >= 3.1.0, < 4.0.0",
             "pycodestyle >= 2.6.0, < 3.0.0",
             "pytest >= 5.0.0, < 6.0.0",
             ],
