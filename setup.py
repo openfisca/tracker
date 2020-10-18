b31fa42440cace5f2e98030fa993a2fc5df87ddd
Mauko Quiroga <hello@mauko.me>
2020-10-19 01:57:38 +0200

    Bump version number in setup.py and CHANGELOG

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name = "OpenFisca-Tracker",
-    version = "0.4.1",
+    version = "0.4.2",
     author = "OpenFisca Team",
     author_email = "contact@openfisca.org",
     classifiers = [
